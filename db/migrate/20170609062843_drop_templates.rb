bd37a66fec08ee82cde5c0ca5a2b171535f53a2e
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-06-09 02:33:46 -0400

    Destroy scaffold for unused tables/components
    Not using templates or template_items

---
+++ db/migrate/20170609062843_drop_templates.rb
@@ -0,0 +1,4 @@
+class DropTemplates < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- scripts/list-items/update-list-item.sh
+++ scripts/list-items/update-list-item.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#LIST_ID=10 ID=17 TOKEN=BAhJIiUzYTIxZmI1ZTdkNzM5MDhhMTc3ZDlmZjg2Y2M5MDY5YQY6BkVG--dc9e27a51eeebe308808967ca2c4bcf133c1b3e1 LIST_ITEM_NAME="durian smells like dirty gross feet" sh scripts/list-items/update-list-item.sh
+#LIST_ID=1 ID=25 TOKEN=BAhJIiU0NjlkOTNiMGNjZDc2MDQ0ZDkxNTQzYjRkYjczMDEyMAY6BkVG--28967b211bbd7ca9f3c75f573fffaa139893b0b6 LIST_ITEM_NAME="tttttttttttt" sh scripts/list-items/update-list-item.sh
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/lists/${LIST_ID}/list_items"
